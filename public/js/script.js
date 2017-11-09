846f0779749148102dea3064329400bfce3deed9
Tejas H S <hstejas@gmail.com>
2017-11-09 15:05:00 -0700

    US Map dummy

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
-//load the data corresponding to all the election years
-//pass this data and instances of all the charts that update on year selection to yearChart's constructor
-d3.csv("data/yearwiseWinner.csv", function (error, electionWinners) {
-    let yearChart = new YearChart(electoralVoteChart, tileChart, votePercentageChart, electionWinners, shiftChart, voterShiftChart);
-    electoralVoteChart.yearChart = yearChart;
-    yearChart.update();
-});
+let usMap = new USMap();
+
+
+  d3.json("https://d3js.org/us-10m.v1.json", function (error, usdata) {
+        if (error) throw error;
+        usMap.drawMap(usdata);
+    });
---
+++ public/js/usmap.js
@@ -0,0 +1,41 @@
+/** Class implementing the votePercentageChart. */
+class USMap {
+
+    /**
+     * Initializes the svg elements required for this chart;
+     */
+    constructor(){
+	    // this.margin = {top: 30, right: 20, bottom: 30, left: 50};
+	    // self.usmapDiv = d3.select("#usmapdiv").classed("content", true);
+      // //fetch the svg bounds
+	    // this.svgBounds = self.usmapDiv.node().getBoundingClientRect();
+	    // this.svgWidth = this.svgBounds.width - this.margin.left - this.margin.right;
+	    // this.svgHeight = 200;
+      //
+	    // //add the svg to the div
+	    // this.svg = self.usmapDiv.select("svg")
+	    //     .attr("width",this.svgWidth)
+	    //     .attr("height",this.svgHeight);
+
+    }
+
+    drawMap(data){
+      console.log(data);
+      this.svgWidth = 800;
+      this.svgHeight = 600;
+      let projection = d3.geoAlbersUsa()  // a USA-specific projection (that deals with Hawaii / Alaska)
+            .translate([this.svgWidth / 2, this.svgHeight / 2]) // this centers the map in our SVG element
+            .scale([700]);
+
+      let path = d3.geoPath().projection(projection);
+      let geodata = topojson.feature(data, data.objects.states);
+      console.log(geodata);
+      var paths = d3.select("#usmap").selectAll("path")
+        .data(geodata.features)
+        .enter()
+        .append("path")
+        .attr("d", path);
+    }
+
+
+}
