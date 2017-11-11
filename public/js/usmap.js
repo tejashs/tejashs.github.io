18479184505a41e89b2fd5b829d6c398ea54b444
Tejas H S <hstejas@gmail.com>
2017-11-10 17:31:30 -0700

    Adding Tooltips for Map

--- public/js/usmap.js
+++ public/js/usmap.js
@@ -13,8 +13,31 @@ class USMap {
       this.path = d3.geoPath().projection(this.projection);
     }
 
+    tooltip_render(tooltip_data) {
+      let text = "<h2>" + tooltip_data.state + " - " + tooltip_data.city + "</h2>";
+      text += "<ul>"
+      text += "<li>"
+      text += "Year - "
+      text += tooltip_data.year
+      text += "</li>"
+      text += "<li>"
+      text += "Target - "
+      text += tooltip_data.target
+      text += "</li>"
+      text += "<li>"
+      text += "Fatalities - "
+      text += tooltip_data.fatalities
+      text += "</li>"
+      text += "<li>"
+      text += "Injuries - "
+      text += tooltip_data.injuries
+      text += "</li>"
+      text += "</ul>";
+      return text;
+    }
+
+
     drawMap(data){
-      console.log(data);
       let self = this;
       d3.select("#usmap").selectAll("path")
       .data(data.features)
@@ -24,20 +47,22 @@ class USMap {
     }
 
     plotStates(data){
-      console.log(data);
       let self = this;
-      d3.select("#circleGroup").selectAll("circle")
+      let crapGps = [];
+      var circles = d3.select("#circleGroup").selectAll("circle")
       .data(data)
       .enter()
       .append("circle")
       .attr("r", 5)
       .attr("cx", function(d){
-        console.log(d);
         let proj = self.projection([d.longitude, d.latitude]);
         if(proj){
           return proj[0];
         }
         else {
+          if(!crapGps.includes(d.state)){
+              crapGps.push(d.state);
+          }
           return 0;
         }
       })
@@ -47,9 +72,29 @@ class USMap {
           return proj[1];
         }
         else {
+          if(!crapGps.includes(d.state)){
+              crapGps.push(d.state);
+          }
           return 0;
         }
       })
       .style("fill", "green");
+
+
+      //for reference:https://github.com/Caged/d3-tip
+      //Use this tool tip element to handle any hover over the chart
+          let tip = d3.tip().attr('class', 'd3-tip')
+              .direction('se')
+              .offset(function() {
+                  return [-100,0];
+              })
+              .html((d)=>{
+                  return self.tooltip_render(d);
+              });
+
+              circles.call(tip);
+              circles.on('mouseover', tip.show).on('mouseout', tip.hide);
+
+
     }
 }
