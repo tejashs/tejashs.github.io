0e4805145b26c6ddb618f33340577ea02d7326a7
Giorgi Kvernadze <gi4_444@hotmail.com>
2017-11-10 19:50:59 -0700

    bar chart

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,20 @@
 let usMap = new USMap();
 
+let barChart = null;
 
 // d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
 d3.json("data/ustopo.json", function(error, us) {
   if (error) throw error;
   usMap.drawMap(us);
 });
+
+d3.csv("data/gtd_by_countries.csv", function(error, data) {
+	if (error) throw error;
+  	barChart = new BarChart(data);
+});
+
+function changeData() {
+    let metric = document.getElementById('metric').value;
+    barChart.updateBarChart(metric)
+
+}
