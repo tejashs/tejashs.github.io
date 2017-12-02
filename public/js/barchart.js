96f36db8d4a18c15687c5f77af4a0b6e61aa8d64
Giorgi Kvernadze <gi4_444@hotmail.com>
2017-12-01 21:56:45 -0700

    barchart horizontal

--- public/js/barchart.js
+++ public/js/barchart.js
@@ -31,27 +31,41 @@ class BarChart {
 
         var svg = d3.select("#barchart")
 
-        let margin = {top: 50, right: 20, bottom: 70, left: 60}
+        let margin = {top: 50, right: 20, bottom: 70, left: 110}
 
         let width = 800 - margin.left - margin.right
         let height = 600 - margin.top - margin.bottom
 
-        let xScale = d3.scaleBand()
-            .range([0, width])
-            .paddingInner(0.05)
-            .domain(data.map(d => d.country))
+        var x = d3.scaleLinear()
+        .range([0, width])
+        .domain([0, d3.max(selectedData)]);
 
-        let yScale = d3.scaleLinear()
-            .range([height, 0])
-            .domain([0, d3.max(selectedData)])
-
+        console.log(d3.max(selectedData))
+        console.log(selectedData)
+        
+        var y = d3.scaleBand()
+        .range([height, 0]);
+        
         let colorScale = d3.scaleLinear()
         .domain([0, d3.max(selectedData)])
         .range(["#D46A6A", "#550000"]);
 
-        var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+        // let xScale = d3.scaleBand()
+        //     .range([0, width])
+        //     .paddingInner(0.05)
+        //     .domain(data.map(d => d.country))
 
-        var xAxis = svg.select("#xAxis")
+        // let yScale = d3.scaleLinear()
+        //     .range([height, 0])
+        //     .domain([0, d3.max(selectedData)])
+
+        var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        x.domain([0, d3.max(data, function(d) { return d[selectedDimension]; })]);
+        y.domain(data.map(function(d) { return d.country; })).padding(0.1);
+
+         var xAxis = svg.select("#xAxis")
         .attr("transform", "translate(" + margin.left + "," + (600 - margin.bottom) + ")")
 
         var yAxis = svg.select("#yAxis")
@@ -59,17 +73,12 @@ class BarChart {
 
         var bars = svg.select("#bars")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        xAxis.call(d3.axisBottom(xScale))
-        .selectAll("text")
-        .attr("dy", "-0.7em")
-        .attr("dx", "1em")
-        .attr("transform", "rotate(90)")
-        .style("text-anchor", "start");
+        
+        xAxis.call(d3.axisBottom(x))
 
         yAxis
         .transition().duration(1000)
-        .call(d3.axisLeft(yScale))
+        .call(d3.axisLeft(y))
 
         var bar = bars.selectAll("rect").data(data).style("opacity", 1)
         bar.exit().transition().duration(1000).style("opacity", 0).remove()
@@ -78,27 +87,89 @@ class BarChart {
 
         mergedBar
         .transition().duration(1000)
-        .attr("x", function(d) {
-            return xScale(d.country);
-        })
+        .attr("x", 0)
         .attr("y", function(d) {
-            return yScale(d[selectedDimension]);
+            return y(d.country);
         })
-        .attr("width", xScale.bandwidth())
-        .attr("height", function(d) {
-            return height - yScale(d[selectedDimension]);
+        .attr("height", y.bandwidth())
+        .attr("width", function(d) {
+            return x(d[selectedDimension]);
         })
         .attr("fill", function(d){
             return colorScale(d[selectedDimension]);
         })
 
-        let tip = d3.tip().attr('class', 'd3-tip').direction('se').offset(function() {
-                    return [-100,0];
-                }).html((d)=>{
-                    return self.getToolTipText(d, selectedDimension);
-                });
-        mergedBar.call(tip);
-        mergedBar.on('mouseover', tip.show).on('mouseout', tip.hide);
+        // g.selectAll("#bar")
+        // .data(data)
+        // .enter().append("rect")
+        // .attr("x", 0)
+        // .attr("height", y.bandwidth())
+        // .attr("y", function(d) { return y(d.country); })
+        // .attr("width", function(d) { return x(d[selectedDimension]); })
+
+        // let xScale = d3.scaleBand()
+        //     .range([0, width])
+        //     .paddingInner(0.05)
+        //     .domain(data.map(d => d.country))
+
+        // let yScale = d3.scaleLinear()
+        //     .range([height, 0])
+        //     .domain([0, d3.max(selectedData)])
+
+        // let colorScale = d3.scaleLinear()
+        // .domain([0, d3.max(selectedData)])
+        // .range(["#D46A6A", "#550000"]);
+
+        // var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
+        // var xAxis = svg.select("#xAxis")
+        // .attr("transform", "translate(" + margin.left + "," + (600 - margin.bottom) + ")")
+
+        // var yAxis = svg.select("#yAxis")
+        // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // var bars = svg.select("#bars")
+        // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // xAxis.call(d3.axisBottom(xScale))
+        // .selectAll("text")
+        // .attr("dy", "-0.7em")
+        // .attr("dx", "1em")
+        // .attr("transform", "rotate(90)")
+        // .style("text-anchor", "start");
+
+        // yAxis
+        // .transition().duration(1000)
+        // .call(d3.axisLeft(yScale))
+
+        // var bar = bars.selectAll("rect").data(data).style("opacity", 1)
+        // bar.exit().transition().duration(1000).style("opacity", 0).remove()
+
+        // var mergedBar = bar.enter().append("rect").merge(bar)
+
+        // mergedBar
+        // .transition().duration(1000)
+        // .attr("x", function(d) {
+        //     return xScale(d.country);
+        // })
+        // .attr("y", function(d) {
+        //     return yScale(d[selectedDimension]);
+        // })
+        // .attr("width", xScale.bandwidth())
+        // .attr("height", function(d) {
+        //     return height - yScale(d[selectedDimension]);
+        // })
+        // .attr("fill", function(d){
+        //     return colorScale(d[selectedDimension]);
+        // })
+
+        // let tip = d3.tip().attr('class', 'd3-tip').direction('se').offset(function() {
+        //             return [-100,0];
+        //         }).html((d)=>{
+        //             return self.getToolTipText(d, selectedDimension);
+        //         });
+        // mergedBar.call(tip);
+        // mergedBar.on('mouseover', tip.show).on('mouseout', tip.hide);
     }
 
     getToolTipText(data, selectedDimension){
