068ce0f5e61ec710c90ac6c662dec1a1e0187e16
Tejas H S <hstejas@gmail.com>
2017-12-01 19:34:20 -0700

    Merging Bar chart and Map with storing region in Util

--- public/js/util.js
+++ public/js/util.js
@@ -3,6 +3,30 @@
   keys["num_attacks"] = "Attacks";
   keys["injuries"] = "Injuries";
 
+  regions = {};
+  regions["Central_America_Caribbean"] = "Central America & Caribbean";
+  regions["North_America"] = "North America";
+  regions["Southeast_Asia"] = "Southeast Asia";
+  regions["Western_Europe"] = "Western Europe";
+  regions["East_Asia"] = "East Asia";
+  regions["South_America"] = "South America";
+  regions["Eastern_Europe"] = "Eastern Europe";
+  regions["Sub-Saharan_Africa"] = "Sub-Saharan Africa";
+  regions["Middle_East_North_Africa"] = "Middle East & North Africa";
+  regions["Australasia_Oceania"] = "Australasia & Oceania";
+  regions["South_Asia"] = "South Asia";
+  regions["Central_Asia"] = "Central Asia";
+
+  currentRegionSelected = null;
+
+  function setRegionSelected(region){
+    currentRegionSelected = regions[region];
+  }
+
+  function getRegionSelected(){
+    return currentRegionSelected;
+  }
+
   function getStringForKey(key){
       if(!keys[key]){
         return key;
@@ -13,7 +37,7 @@
   }
 
   function getTopYearInfo(countryData){
-    
+
     var tgroups = groupBy(countryData, "tgroup")
     var targets = groupBy(countryData, "target")
     var weapons = groupBy(countryData, "weapon")
@@ -34,6 +58,6 @@
 
     if (group.length > 10)
       group = group.slice(0, 10)
-    
+
     return group
   }
